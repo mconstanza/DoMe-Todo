1c74144ec3d315a0ef56ecc986648c2722670d2c
Michael Constanza <Michael Constanza>
2016-11-08 10:04:59 -0500

    changed connect.js to work on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,14 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host:'localhost',
-  user: 'root',
-  password: 'Batman626',
+  host:'ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'clbadcs2f8mabf4w',
+  password: 'n2hb9oqywz3lcs1e',
   database: 'todo_db'
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-
-  console.log('connected as id ' + connection.threadId);
 })
 
 module.exports = connection;
